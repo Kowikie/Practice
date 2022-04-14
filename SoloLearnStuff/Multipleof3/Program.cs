f4ecb957a26bc0b5f08a74c99e55316d515a45db
Kowikie <psthames@gmail.com>
2022-04-13 20:19:16 -0600

    SoloLearn Projects 1&2

---
+++ SoloLearnStuff/Multipleof3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 1; x <= number; x++)
+            {
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine("*");
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+            }
+
+        }
+    }
+}
