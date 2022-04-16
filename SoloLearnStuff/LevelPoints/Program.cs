2d5242888fe0e2e2224a39295c556139859c23d1
Kowikie <psthames@gmail.com>
2022-04-15 18:20:25 -0600

    Level Points

---
+++ SoloLearnStuff/LevelPoints/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Points(levels));
+        }
+
+        static int Points(int levels)
+        {
+           if (levels == 1)
+                {return 1;}
+           else
+           {
+               return levels + Points(levels - 1);
+           }
+        }
+    }
+}
