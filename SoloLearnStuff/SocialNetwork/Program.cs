5c5a2d274c76b59a3eb5a978ad52c14e348de38f
Kowikie <psthames@gmail.com>
2022-04-17 10:02:25 -0600

    Solo Learn Classes & Objects

---
+++ SoloLearnStuff/SocialNetwork/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string postText = Console.ReadLine();
+
+            Post post = new Post();
+            post.Text = postText;
+            post.ShowPost();
+        }
+    }
+    class Post
+    {
+        private string text;
+
+        public Post()
+        {
+            Console.WriteLine("New post");
+        }
+
+        public string Text
+        {
+            get { return text;}
+            set {text = value;}
+        }
+
+        public void ShowPost()
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
